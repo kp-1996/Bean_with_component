50277418fdaec220f6a742ae5f9d71ddf16de9f8
Prasada <Prasada.kp@outlook.com>
2020-04-27 13:11:47 +0530

    test

---
+++ src/main/java/com/bean/demo/BeanWithComponentApplication.java
@@ -0,0 +1,13 @@
+package com.bean.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanWithComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanWithComponentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bean/demo/BeanWithComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bean.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanWithComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
